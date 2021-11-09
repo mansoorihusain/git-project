52abb1cf3a7ab2ec2fca5e286850c4cbb9a0ecc2
mansoorihusain <mansoorihusain9171@gmail.com>
2021-11-09 21:29:55 +0530

    project send successfull

---
+++ sort.java
@@ -0,0 +1,20 @@
+import javax.print.event.PrintEvent;
+
+class Sample{
+    public static void main(String[] args) {
+        int arr[] = {4,7,2,9,8,1,5,6};
+        int temp;
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]>arr[j]){
+                    temp  = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp; 
+                }
+            }
+        }
+        for(int i : arr){
+            System.out.print(i+" ");
+        }
+    }
+}
